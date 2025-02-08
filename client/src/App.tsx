567a962b62f3e9bd3eddb3a4312c2f581fa0e08c
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2025-02-08 23:26:38 +0200

    test commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const fetchData = async () => {
+    const response = await fetch("https://planit-neqe.onrender.com/api/users"); // Replace with your live server's URL
+    const data = await response.json();
+    console.log(data);
+
+    setUser(data[0]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <h1>PLANTit</h1>
       <h2>In production!</h2>
+      <h3>{user?.email}</h3>
     </>
   );
 }

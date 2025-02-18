477ce736202c36ee5c52e53b15afb555cb9a0e22
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2025-02-18 23:32:06 +0200

    bugfix

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,10 +13,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-  // origin:
-  //   process.env.NODE_ENV === "production"
-  //     ? process.env.CLIENT_URL
-  //     : "http://localhost:5173",
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.CLIENT_URL
+      : "http://localhost:5173",
   credentials: true, // Allow cookies to be sent
 };
 

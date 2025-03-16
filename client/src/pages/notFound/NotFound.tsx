3d17dc6fb5cb2360c4f5f493aca2e8fe4e1836f5
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2025-03-16 22:29:55 +0200

    bugfix: fixed image routing

--- client/src/pages/notFound/NotFound.tsx
+++ client/src/pages/notFound/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
     <NotFoundContainer>
       <NotFoundTitle>404 - Page Not Found</NotFoundTitle>
       <NotFoundText>The page you are looking for does not exist.</NotFoundText>
-      <NotFoundImage src="404-logo.png" alt="404 illustration" />
+      <NotFoundImage src="/404-logo.png" alt="404 illustration" />
       <HomeLink to="/">Go back home</HomeLink>
     </NotFoundContainer>
   );

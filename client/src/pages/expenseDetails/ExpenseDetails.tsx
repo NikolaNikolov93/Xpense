1e6a54e4a9fdbc9392df66741f5c169792d5b08b
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2025-03-30 21:19:22 +0300

    style: style fix

--- client/src/pages/expenseDetails/ExpenseDetails.tsx
+++ client/src/pages/expenseDetails/ExpenseDetails.tsx
@@ -3,10 +3,13 @@ import { ExpenseDetailsWrapper } from "./ExpenseDetails.styles";
 import { useFetchExpense } from "../../hooks/useFetchExpenseDetails";
 import Spinner from "../../components/spinner/Spinner";
 import EmptyData from "../../components/emptyData/EmptyData";
+import { RootState } from "../../redux/store";
+import { useSelector } from "react-redux";
 
 const ExpenseDetails = () => {
   const { expenseId } = useParams<{ expenseId: string }>();
   const { data: expense, isPending, error } = useFetchExpense(expenseId!);
+  const user = useSelector((state: RootState) => state.user.user);
 
   if (isPending)
     return (
@@ -20,7 +23,7 @@ const ExpenseDetails = () => {
   return (
     <ExpenseDetailsWrapper>
       <h2>{expense?.title}</h2>
-      <p>Amount: {expense?.amount}</p>
+      <p>{`Amount: ${expense.amount} ${user?.currency}`}</p>
 
       <p>Category: {expense?.category}</p>
     </ExpenseDetailsWrapper>
